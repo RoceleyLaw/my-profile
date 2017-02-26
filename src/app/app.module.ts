4bd150bb7fcdfde9b6b361c92c568fa4e6f94642
roceleylaw <roceleylaw@gmail.com>
2017-02-26 18:39:15 -0500

    simple splash and main pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SplashPageComponent } from './splash-page/splash-page.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    SplashPageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'main-page',
+        component: MainPageComponent
+      },
+      {
+        path: 'splash-page',
+        component: SplashPageComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
